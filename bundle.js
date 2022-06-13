3ac643fed6765047ff63460ba1bbda7860707951
Guilherme <cryptosalomao@gmail.com>
2022-06-13 17:18:16 -0300

    remove first floor

--- bundle.js
+++ bundle.js
@@ -5,7 +5,7 @@ const container = document.getElementById('viewer');
 
 const viewer = new PhotoSphereViewer({
   container,
-  panorama: './img/1-andar.jpg',
+  panorama: './img/5-andar.jpg',
   navbar: [
     'autorotate',
     'zoom',
